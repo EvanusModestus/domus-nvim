df776e59b70e79e2b7ae986e3e642b8731c75d5a
EvanusModestus <evan.rosado@outlook.com>
2026-02-12 22:36:36 -0800

    feat: add noice.nvim for enhanced UI
    - Floating cmdline (command_palette preset)
    - Better message routing (skip 'written' and search_count)
    - LSP signature/doc styling
    - nvim-notify with compact render

--- lua/domus/plugins/specs/ui.lua
+++ lua/domus/plugins/specs/ui.lua
@@ -136,6 +136,42 @@ return {
         },
     },
 
+    -- Noice (better UI for messages, cmdline, notifications)
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        config = function()
+            require("noice").setup({
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                    },
+                },
+                presets = {
+                    bottom_search = true,
+                    command_palette = true,
+                    long_message_to_split = true,
+                    lsp_doc_border = true,
+                },
+                routes = {
+                    { filter = { event = "msg_show", kind = "", find = "written" }, opts = { skip = true } },
+                    { filter = { event = "msg_show", kind = "search_count" }, opts = { skip = true } },
+                },
+            })
+            require("notify").setup({
+                background_colour = "#1a1a26",
+                render = "compact",
+                stages = "fade",
+                timeout = 2000,
+            })
+        end,
+    },
+
     -- Better UI for vim.ui.select and vim.ui.input
     {
         "stevearc/dressing.nvim",
