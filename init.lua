47af28d6064928e2a25096c96b0a3d812f757338
EvanusModestus <evan.rosado@outlook.com>
2026-03-16 13:48:12 -0700

    fix: Add neovim 0.11+ requirement check and update README
    - init.lua: Add version check with clear error message and install instructions
    - README.adoc: Rename from Domus Instrumentum to domus-nvim
    - README.adoc: Add Requirements section (neovim 0.11+, git, node, ripgrep)
    - README.adoc: Add installation instructions for Ubuntu, Arch, macOS
    - README.adoc: Update clone URLs to EvanusModestus/domus-nvim
    
    Prevents confusing errors when users run with older neovim versions.

--- init.lua
+++ init.lua
@@ -8,4 +8,17 @@
 --   domus.features - Custom systems (MOTD, regex-mastery, captures)
 --   domus.ui       - Visual components (colorscheme, statusline)
 
+-- Version check: mason-lspconfig requires neovim 0.11+
+local min_version = "0.11.0"
+if vim.fn.has("nvim-0.11") ~= 1 then
+  vim.api.nvim_echo({
+    { "domus-nvim requires Neovim " .. min_version .. "+\n", "ErrorMsg" },
+    { "Current version: " .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch .. "\n", "WarningMsg" },
+    { "\nInstall latest neovim:\n", "Normal" },
+    { "  sudo add-apt-repository ppa:neovim-ppa/unstable\n", "Comment" },
+    { "  sudo apt update && sudo apt install neovim\n", "Comment" },
+  }, true, {})
+  return
+end
+
 require("domus")
